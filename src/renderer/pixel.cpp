419da07714de5df9bd0f5f0f8c059ec1eab28166
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2024-03-24 00:24:01 +0530

    split declaration and implementation

--- src/renderer/pixel.cpp
+++ src/renderer/pixel.cpp
@@ -4,6 +4,30 @@
 
 #include "pixel.h"
 
+Uint8 Pixel::r() const {
+    return data & 0xFF;
+}
+
+Uint8 Pixel::g() const {
+    return data >> 8 & 0xFF;
+}
+
+Uint8 Pixel::b() const {
+    return data >> 16 & 0xFF;
+}
+
+Uint8 Pixel::a() const {
+    return data >> 24 & 0xFF;
+}
+
+Pixel Pixel::operator/(const Uint8 value) const {
+    return {Uint8(r() / value), Uint8(g() / value), Uint8(b() / value), Uint8(a() / value)};
+}
+
+Pixel Pixel::operator*(const double value) const {
+    return {Uint8(r() * value), Uint8(g() * value), Uint8(b() * value), Uint8(a() * value)};
+}
+
 void Pixel::a(const Uint8 alpha) {
     data = (data & 0x00FFFFFF) | (alpha << 24);
 }
--- src/renderer/pixel.h
+++ src/renderer/pixel.h
@@ -15,33 +15,21 @@ class Pixel {
     static Uint32 pack(Uint8 r, Uint8 g, Uint8 b, Uint8 a) { return (r | g << 8 | b << 16 | a << 24); }
 
 public:
-    Pixel(const Uint8 r, const Uint8 g, const Uint8 b, const Uint8 a): data(Pixel::pack(r, g, b, a)) {}
+    Pixel(const Uint8 r, const Uint8 g, const Uint8 b, const Uint8 a) : data(Pixel::pack(r, g, b, a)) {};
 
-    Pixel(const Uint8 r, const Uint8 g, const Uint8 b) : Pixel(r, g, b, 255) {}
+    Pixel(const Uint8 r, const Uint8 g, const Uint8 b) : Pixel(r, g, b, 255) {};
 
-    [[nodiscard]] Uint8 r() const {
-        return data & 0xFF;
-    };
+    [[nodiscard]] Uint8 r() const;
 
-    [[nodiscard]] Uint8 g() const {
-        return data >> 8 & 0xFF;
-    };
+    [[nodiscard]] Uint8 g() const;
 
-    [[nodiscard]] Uint8 b() const {
-        return data >> 16 & 0xFF;
-    };
+    [[nodiscard]] Uint8 b() const;
 
-    [[nodiscard]] Uint8 a() const {
-        return data >> 24 & 0xFF;
-    };
+    [[nodiscard]] Uint8 a() const;
 
-    Pixel operator/(const Uint8 value) const {
-        return {Uint8(r() / value), Uint8(g() / value), Uint8(b() / value), Uint8(a() / value)};
-    }
+    Pixel operator/(const Uint8 value) const;
 
-    Pixel operator*(const double value) const {
-        return {Uint8(r() * value), Uint8(g() * value), Uint8(b() * value), Uint8(a() * value)};
-    }
+    Pixel operator*(const double value) const;
 
     void a(const Uint8 alpha);
 
