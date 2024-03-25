9fc84a040e68500d3ac6432dd8a9d96272d08d01
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2024-03-25 15:48:12 +0530

    add Enemy config

--- src/base/config.h
+++ src/base/config.h
@@ -21,11 +21,19 @@
 #define DEFAULT_FONT_SIZE 24
 
 #define PLAYER_SHAPE_RADIUS 60
-#define PLAYER_SPEED 1.0
+#define PLAYER_SPEED 1.0 // Units / frame
 #define PLAYER_FILL_COLOR {0, 0, 255}
 #define PLAYER_OUTLINE_COLOR {0, 255, 0}
 #define PLAYER_OUTLINE_THICKNESS 8
 
+#define ENEMY_SHAPE_RADIUS_MIN 20
+#define ENEMY_SHAPE_RADIUS_MAX 50
+#define ENEMY_SPEED_MIN 2.0 // Units / frame
+#define ENEMY_SPEED_MAX 4.0 // Units / frame
+#define ENEMY_OUTLINE_COLOR {255, 0, 0}
+#define ENEMY_THICKNESS 4
+#define ENEMY_SPAWN_INTERVAL 1000 // Frames
+#define ENEMY_FRAGMENTS_LIFESPAN 800 // Frames
 struct Window {
     int width = WIDTH;
     int height = HEIGHT;
@@ -51,16 +59,29 @@ struct Player {
     int thickness = PLAYER_OUTLINE_THICKNESS;
 };
 
+struct Enemy {
+    std::pair<int, int> shapeRadius = {ENEMY_SHAPE_RADIUS_MIN, ENEMY_SHAPE_RADIUS_MAX};
+    std::pair<float, float> speed = {ENEMY_SPEED_MIN, ENEMY_SPEED_MAX};
+
+    Pixel outlineColor = ENEMY_OUTLINE_COLOR;
+
+    int thickness = ENEMY_THICKNESS;
+    int fragmentLifespan = ENEMY_FRAGMENTS_LIFESPAN;
+    int spawnInterval = ENEMY_SPAWN_INTERVAL;
+};
+
 class Config {
-    explicit Config(const Window &window, const Font &font, const Player &player) :
+    explicit Config(const Window &window, const Font &font, const Player &player, const Enemy &enemy) :
             window(window),
             font(font),
-            player(player) {};
+            player(player),
+            enemy(enemy) {};
 
 public:
     const Window window;
     const Font font;
     const Player player;
+    const Enemy enemy;
 
     static Config loadFromFile(const std::string &filename);
 };
