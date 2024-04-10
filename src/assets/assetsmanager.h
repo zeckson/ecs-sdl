da6f270151927006455b78bddd38f2ff8ed99cee
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2024-04-10 21:50:34 +0530

    refactor/move

--- src/assets/assetsmanager.h
+++ src/assets/assetsmanager.h
@@ -15,7 +15,7 @@
 #define TEXTURE_TYPE "Texture"
 
 class AssetsManager {
-    std::map<std::string, Animation> animations;
+    std::map<std::string, std::unique_ptr<Animation>> animations;
     std::map<std::string, std::unique_ptr<Texture>> textures;
     std::map<std::string, TTF_Font*> fonts;
 
@@ -23,20 +23,19 @@ class AssetsManager {
 public:
     explicit AssetsManager() {}
 
-    void addTexture(std::string name, Texture texture) {
-        textures[name] = std::make_unique<Texture>(texture);
+    void addTexture(const std::string &name, SDL_Texture *texture, SDL_Surface *surface) {
+        textures[name] = std::make_unique<Texture>(name, texture, surface);
     };
-
-
-    static const AssetsManager load(const std::string &filename);
 
     void addFont(const std::string &name, TTF_Font *pFont) {
         fonts[name] = pFont;
     };
 
+    TTF_Font *getFont(const std::string &string) const;
+
     void quit();
 
-    TTF_Font *getFont(const std::string &string) const;
+    static const AssetsManager load(const std::string &filename);
 };
 
 
