24231535676294169f6ba25d3b279fe55bff38f5
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2024-04-11 18:41:46 +0530

    cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -3,19 +3,11 @@
 #include "engine/game.h"
 #include "resource/config.h"
 #include "gameofballs/ballgame.h"
-#include "resource/logger.h"
 #include "spaceships/spaceshipgame.h"
 
-//int main() {
-//    BallGame game("The Game Of Balls");
-//
-//    game.start();
-//
-//    return 0;
-//}
-
 int main() {
-    SpaceshipGame game("Spaceships", Config::loadFromFile("conf/game.conf"));
+    BallGame game("The Game Of Balls");
+//    SpaceshipGame game("Spaceships", Config::loadFromFile("conf/game.conf"));
 
     game.start();
 
