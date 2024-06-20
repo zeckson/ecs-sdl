516f46eb154cd1fcdee00f6bea43bb191716c87d
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2024-06-20 15:13:23 +0700

    add action

---
+++ engine/action/action.h
@@ -0,0 +1,22 @@
+//
+// Created by Evgenii Shchepotev on 20.06.2024.
+//
+
+#ifndef ECS_ACTION_H
+#define ECS_ACTION_H
+
+#include <string>
+
+enum ActionType {
+  UNKNOWN = 0, PRESS, RELEASE
+};
+
+class Action {
+ public:
+  const ActionType type;
+  const std::string& name;
+
+  Action(const std::string& name, const ActionType type) : name(name), type(type) {}
+};
+
+#endif  // ECS_ACTION_H
