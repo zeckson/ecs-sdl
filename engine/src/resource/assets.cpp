d29bfab6147db6dd02cd4676a69a58f219c1bd3e
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2025-01-26 12:22:05 +0530

    format

--- engine/src/resource/assets.cpp
+++ engine/src/resource/assets.cpp
@@ -3,52 +3,52 @@
 namespace Asset {
 
 void Window::load(std::ifstream& in) {
-    in >> width >> height >> fps >> fullscreen;
-    if (in.fail()) {
-        throw std::runtime_error("Reading Window resource failed");
-    }
+  in >> width >> height >> fps >> fullscreen;
+  if (in.fail()) {
+    throw std::runtime_error("Reading Window resource failed");
+  }
 }
 
 void Sprite::load(std::ifstream& in) {
-    in >> name >> path >> angle;
-    if (in.fail()) {
-        throw std::runtime_error("Reading Sprite resource failed");
-    }
+  in >> name >> path >> angle;
+  if (in.fail()) {
+    throw std::runtime_error("Reading Sprite resource failed");
+  }
 }
 
 void Font::load(std::ifstream& in) {
-    in >> name >> path >> size;
-    if (in.fail()) {
-        throw std::runtime_error("Reading Font resource failed");
-    }
+  in >> name >> path >> size;
+  if (in.fail()) {
+    throw std::runtime_error("Reading Font resource failed");
+  }
 }
 
 void Animation::load(std::ifstream& in) {
-    Sprite::load(in);  // Load base Sprite data
-    in >>  frames >> lifetime;
-    if (in.fail()) {
-        throw std::runtime_error("Reading Animation failed");
-    }
+  Sprite::load(in);  // Load base Sprite data
+  in >> frames >> lifetime;
+  if (in.fail()) {
+    throw std::runtime_error("Reading Animation failed");
+  }
 }
 
 }  // namespace Asset
 
-Assets::Assets(): resources({}) {
-    // Register loaders for different resource types
-    loaders["Window"] = []() { return std::make_shared<Asset::Window>(); };
-    loaders["Font"] = []() { return std::make_shared<Asset::Font>(FONT_NAME); };
-    loaders["Texture"] = []() { return std::make_shared<Asset::Sprite>(); };
-    loaders["Animation"] = []() { return std::make_shared<Asset::Animation>(); };
+Assets::Assets() : resources({}) {
+  // Register loaders for different resource types
+  loaders["Window"] = []() { return std::make_shared<Asset::Window>(); };
+  loaders["Font"] = []() { return std::make_shared<Asset::Font>(FONT_NAME); };
+  loaders["Texture"] = []() { return std::make_shared<Asset::Sprite>(); };
+  loaders["Animation"] = []() { return std::make_shared<Asset::Animation>(); };
 }
 
 Assets Assets::instance = Assets();
 
 template <typename T>
 const T& Assets::getResource(const std::string& name) {
-    auto resource = resources.find(name);
-    if (resource == resources.end()) {
-        throw std::runtime_error("No resource found with name: " + name);
-    }
+  auto resource = resources.find(name);
+  if (resource == resources.end()) {
+    throw std::runtime_error("No resource found with name: " + name);
+  }
   return resource->second;
 }
 
@@ -66,7 +66,7 @@ const Assets& Assets::load(const std::string& path) {
     fin >> type;
     auto loader = instance.loaders.find(type);
     if (loader == instance.loaders.end()) {
-        throw std::runtime_error("No loader registered for type: " + type);
+      throw std::runtime_error("No loader registered for type: " + type);
     }
     auto resource = loader->second();
 
@@ -78,4 +78,3 @@ const Assets& Assets::load(const std::string& path) {
 
   return instance;
 }
-
