7b906556cf56f59596443b51ecf8cc78b98100ac
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2025-01-27 16:51:58 +0530

    add indent

--- engine/src/scene/menuscene.cpp
+++ engine/src/scene/menuscene.cpp
@@ -3,6 +3,11 @@
 #include <renderer/textrenderer.h>
 #include <resource/logger.h>
 
+const Pixel GRAY = Pixel{100, 100, 100, 255};
+
+#define MENU_ITEM_HEIGHT 40
+#define MENU_ITEM_INDENT 10
+
 MenuScene::MenuScene(const std::vector<std::string>& items, SceneGame& game)
     : menuItems(items), game(game), selectedItem(0) {}
 
@@ -15,25 +20,26 @@ void MenuScene::init() {
 }
 
 void MenuScene::onKeyboardAction(const Action& action) {
-  if (action.name == "UP") {
-    selectedItem = (selectedItem - 1 + menuItems.size()) % menuItems.size();
-  } else if (action.name == "DOWN") {
-    selectedItem = (selectedItem + 1) % menuItems.size();
-  } else if (action.name == "RETURN") {
-    Logger::info("Selected item: %s", menuItems[selectedItem].c_str());
+  if (action.type == ActionType::END) {
+    if (action.name == "UP") {
+      selectedItem = (selectedItem - 1 + menuItems.size()) % menuItems.size();
+    } else if (action.name == "DOWN") {
+      selectedItem = (selectedItem + 1) % menuItems.size();
+    } else if (action.name == "RETURN") {
+      Logger::info("Selected item: %s", menuItems[selectedItem].c_str());
+    }
   }
 }
 
 void MenuScene::onMouseAction(const MouseAction& action) {}
 
-const Pixel GRAY = Pixel{100, 100, 100, 255};
-
 void MenuScene::update() {
   auto& renderer = game.renderer;
 
-  int menuWidth = game.width / 2;
+  int menuStartX = game.width / 4;
   int menuStartY = game.height / 4;
-  int menuItemHeight = 40;
+  int menuItemWidth = game.width / 2;
+  int menuItemHeight = MENU_ITEM_HEIGHT;
 
   auto font = game.getAssetsManager().getFont("glitchgoblin");
   auto textBuilder = TextRenderer::Builder(font);
@@ -41,8 +47,11 @@ void MenuScene::update() {
   for (size_t i = 0; i < menuItems.size(); ++i) {
     Pixel background = (i == selectedItem) ? WHITE : GRAY;
     Pixel textColor = (i == selectedItem) ? Pixel{255, 0, 0, 255} : BLACK;
-    const SDL_Rect box = {game.width / 2 - menuWidth / 2, menuStartY + static_cast<int>(i) * menuItemHeight, menuWidth,
-                          menuItemHeight};
+
+    int y = menuStartY + static_cast<int>(i) * (menuItemHeight + MENU_ITEM_INDENT);
+
+    const SDL_Rect box = {menuStartX, y, menuItemWidth, menuItemHeight};
+
     renderer->render(textBuilder.withText(menuItems[i])
                          .withColor(textColor)
                          .withBackgroundColor(background)
